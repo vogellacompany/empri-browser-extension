96758de3511374ebe48a5a08bab33c8d816dacdd
fap <fapdash@users.noreply.github.com>
2020-10-07 17:45:41 +0200

    Run prettier on all .js and .json files

--- options/options.js
+++ options/options.js
@@ -1,17 +1,17 @@
 function saveOptions(e) {
   browser.storage.sync.set({
-    redactionPattern: document.querySelector("#redaction-pattern").value
+    redactionPattern: document.querySelector("#redaction-pattern").value,
   });
   e.preventDefault();
 }
 
 function restoreOptions() {
-  var gettingItem = browser.storage.sync.get('redactionPattern');
+  var gettingItem = browser.storage.sync.get("redactionPattern");
   gettingItem.then((res) => {
-    document.querySelector("#redaction-pattern").value = res.redactionPattern || 'hms';
+    document.querySelector("#redaction-pattern").value =
+      res.redactionPattern || "hms";
   });
 }
 
-
-document.addEventListener('DOMContentLoaded', restoreOptions);
+document.addEventListener("DOMContentLoaded", restoreOptions);
 document.querySelector("form").addEventListener("submit", saveOptions);
--- popup/popup.js
+++ popup/popup.js
@@ -1,39 +1,42 @@
 function setButtonClass() {
-    browser.storage.sync.get('ghrOn').then((res) => {
-        let onOffButton = document.querySelector(".on-off");
-        if (res.ghrOn) {
-            onOffButton.classList.remove("off");
-            onOffButton.classList.add("on");
-        } else {
-            onOffButton.classList.remove("on");
-            onOffButton.classList.add("off");
-        }
-    });
+  browser.storage.sync.get("ghrOn").then((res) => {
+    let onOffButton = document.querySelector(".on-off");
+    if (res.ghrOn) {
+      onOffButton.classList.remove("off");
+      onOffButton.classList.add("on");
+    } else {
+      onOffButton.classList.remove("on");
+      onOffButton.classList.add("off");
+    }
+  });
 }
 setButtonClass();
 
 function toggleOnOff(e) {
-    // TODO(FAP): change browser bar icon when deactivated?
-    let button = e.currentTarget;
-    browser.storage.sync.get('ghrOn').then((res) => {
-        browser.storage.sync.set({
-            ghrOn: !res.ghrOn
-        }).then(() => setButtonClass());
-    })
+  // TODO(FAP): change browser bar icon when deactivated?
+  let button = e.currentTarget;
+  browser.storage.sync.get("ghrOn").then((res) => {
+    browser.storage.sync
+      .set({
+        ghrOn: !res.ghrOn,
+      })
+      .then(() => setButtonClass());
+  });
 }
 
 function onOpened() {
-    console.log(`Options page opened`);
-    window.close();
+  console.log(`Options page opened`);
+  window.close();
 }
 function onError(error) {
-    console.log(`Error: ${error}`);
+  console.log(`Error: ${error}`);
 }
 function openSettings() {
-    var opening = browser.runtime.openOptionsPage();
-    opening.then(onOpened, onError);
+  var opening = browser.runtime.openOptionsPage();
+  opening.then(onOpened, onError);
 }
 
-document.querySelector(".settings-button").addEventListener("click", openSettings);
+document
+  .querySelector(".settings-button")
+  .addEventListener("click", openSettings);
 document.querySelector(".on-off").addEventListener("click", toggleOnOff);
-
