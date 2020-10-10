c47a6726e9f52b7068d4dc1521b5c6356440ea0e
fap <fapdash@users.noreply.github.com>
2020-10-11 01:02:00 +0200

    Redact timestamps on PR & Commit timeline pages

--- content_scripts/redact.js
+++ content_scripts/redact.js
@@ -40,9 +40,6 @@
           if (res.ghrRedactSeconds) {
             dateTime = dateTime.startOf("second");
           }
-          console.log("####");
-          console.log(el);
-          console.log(dateTime);
           el.setAttribute("datetime", dateTime.toISO());
           el.setAttribute("redacted", true);
         });
@@ -56,6 +53,53 @@
 
     console.log("Redacting timestamps");
     redactTimestamps();
+
+    function redactTimelineItem(el) {
+      browser.storage.sync
+        .get([
+          "ghrRedactMonth",
+          "ghrRedactDay",
+          "ghrRedactHours",
+          "ghrRedactMinutes",
+          "ghrRedactSeconds",
+        ])
+        .then((res) => {
+          // "Commits on Jul 12, 2020".substring(11) -> "Jul 12, 2020"
+          let text = el.textContent.substring(11);
+          let dateTime = DateTime.fromFormat(text, "MMM d, y");
+          if (res.ghrRedactMonth) {
+            dateTime = dateTime.startOf("month");
+          }
+          if (res.ghrRedactDay) {
+            dateTime = dateTime.startOf("day");
+          }
+          if (res.ghrRedactHours) {
+            dateTime = dateTime.startOf("hour");
+          }
+          if (res.ghrRedactMinutes) {
+            dateTime = dateTime.startOf("minute");
+          }
+          if (res.ghrRedactSeconds) {
+            dateTime = dateTime.startOf("second");
+          }
+          console.log("####");
+          console.log(el);
+          console.log(dateTime);
+          el.textContent = "Commits on " + dateTime.toFormat("MMM d, y");
+          el.setAttribute("redacted", true);
+        });
+    }
+
+    // https://github.com/user/repo/commits/master
+    // https://github.com/user/repo/pull/42/commits
+    function redactTimelineTimestamps() {
+      document.querySelectorAll(".TimelineItem-body > .f5").forEach((el) => {
+        redactTimelineItem(el);
+        // TODO(FAP): join days that show the same date now?
+      });
+    }
+
+    redactTimelineTimestamps();
 
     console.log("Initializing git-privacy MutationObserver");
     let observer = new MutationObserver((mutations) => {
@@ -71,10 +115,6 @@
         }
       });
     });
-
-    // TODO(FAP): handle additonal timestamps:
-    // https://github.com/akermu/emacs-libvterm/commits/master
-    // https://github.com/akermu/emacs-libvterm/pull/395/commits
 
     observer.observe(document, {
       childList: true,
