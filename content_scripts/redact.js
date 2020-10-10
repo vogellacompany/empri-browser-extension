06089e1ed7397322f0f83b142e685fccb99882ab
fap <fapdash@users.noreply.github.com>
2020-10-10 23:14:15 +0200

    Don't redact the timestamps if extension is "off" in the settings

--- content_scripts/redact.js
+++ content_scripts/redact.js
@@ -14,76 +14,73 @@
       console.log("Git-privacy deactivated");
       return;
     }
-  });
+    function redact(el) {
+      browser.storage.sync
+        .get([
+          "ghrRedactMonth",
+          "ghrRedactDay",
+          "ghrRedactHours",
+          "ghrRedactMinutes",
+          "ghrRedactSeconds",
+        ])
+        .then((res) => {
+          let dateTime = DateTime.fromISO(el.getAttribute("datetime"));
+          if (res.ghrRedactMonth) {
+            dateTime = dateTime.startOf("month");
+          }
+          if (res.ghrRedactDay) {
+            dateTime = dateTime.startOf("day");
+          }
+          if (res.ghrRedactHours) {
+            dateTime = dateTime.startOf("hour");
+          }
+          if (res.ghrRedactMinutes) {
+            dateTime = dateTime.startOf("minute");
+          }
+          if (res.ghrRedactSeconds) {
+            dateTime = dateTime.startOf("second");
+          }
+          console.log("####");
+          console.log(el);
+          console.log(dateTime);
+          el.setAttribute("datetime", dateTime.toISO());
+          el.setAttribute("redacted", true);
+        });
+    }
 
-  function redact(el) {
-    browser.storage.sync
-      .get([
-        "ghrRedactMonth",
-        "ghrRedactDay",
-        "ghrRedactHours",
-        "ghrRedactMinutes",
-        "ghrRedactSeconds",
-      ])
-      .then((res) => {
-        let dateTime = DateTime.fromISO(el.getAttribute("datetime"));
-        if (res.ghrRedactMonth) {
-          dateTime = dateTime.startOf("month");
-        }
-        if (res.ghrRedactDay) {
-          dateTime = dateTime.startOf("day");
-        }
-        if (res.ghrRedactHours) {
-          dateTime = dateTime.startOf("hour");
-        }
-        if (res.ghrRedactMinutes) {
-          dateTime = dateTime.startOf("minute");
-        }
-        if (res.ghrRedactSeconds) {
-          dateTime = dateTime.startOf("second");
-        }
-
-        el.setAttribute("datetime", dateTime.toISO());
-        el.setAttribute("redacted", true);
-      });
-  }
-
-  console.log("Redacting timestamps");
-
-  function redactTimestamps() {
-    Array.from(document.querySelectorAll("time-ago, relative-time")).forEach(
-      (el) => {
+    function redactTimestamps() {
+      document.querySelectorAll("time-ago, relative-time").forEach((el) => {
         redact(el);
-      }
-    );
-  }
+      });
+    }
 
-  redactTimestamps();
+    console.log("Redacting timestamps");
+    redactTimestamps();
 
-  console.log("Initializing git-privacy MutationObserver");
+    console.log("Initializing git-privacy MutationObserver");
+    let observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (!mutation.target) return;
 
-  let observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-      if (!mutation.target) return;
-
-      let node = mutation.target;
-      if (
-        (node.nodeName === "TIME-AGO" || node.nodeName === "RELATIVE-TIME") &&
-        !node.getAttribute("redacted")
-      ) {
-        redact(node);
-      }
+        let node = mutation.target;
+        if (
+          (node.nodeName === "TIME-AGO" || node.nodeName === "RELATIVE-TIME") &&
+          !node.getAttribute("redacted")
+        ) {
+          redact(node);
+        }
+      });
     });
-  });
 
-  // TODO(FAP): handle additonal timestamps:
-  // https://github.com/akermu/emacs-libvterm/commits/master
-  // https://github.com/akermu/emacs-libvterm/pull/395/commits
+    // TODO(FAP): handle additonal timestamps:
+    // https://github.com/akermu/emacs-libvterm/commits/master
+    // https://github.com/akermu/emacs-libvterm/pull/395/commits
 
-  observer.observe(document, {
-    childList: true,
-    subtree: true,
-    attributes: false,
-    characterData: false,
+    observer.observe(document, {
+      childList: true,
+      subtree: true,
+      attributes: false,
+      characterData: false,
+    });
   });
 })();
