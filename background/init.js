4ca56009a0d4586dac33fbe50e33118320e92066
fap <fapdash@users.noreply.github.com>
2020-10-23 19:52:21 +0200

    Set redaction pattern to "hms" on first load

--- background/init.js
+++ background/init.js
@@ -2,9 +2,13 @@ browser.storage.sync.get("ghrOn").then((res) => {
   if (typeof res.ghrOn == "undefined") {
     browser.storage.sync.set({
       ghrOn: true,
+      ghrRedactMonth: false,
+      ghrRedactDay: false,
+      ghrRedactHours: true,
+      ghrRedactMinutes: true,
+      ghrRedactSeconds: true,
     });
   } else if (!res.ghrOn) {
     browser.browserAction.setIcon({ path: "../icons/off.svg" });
   }
-  // TODO(FAP): set initial pattern to "hms" on first load?
 });
