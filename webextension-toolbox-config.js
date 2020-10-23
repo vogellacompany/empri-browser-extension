963e4993bbdb7501257a886deaf44d7bd53c7c92
fap <fapdash@users.noreply.github.com>
2020-10-23 23:08:39 +0200

    Add custom config that applies the webextension polyfill for edge
    see
    - https://github.com/webextension-toolbox/webextension-toolbox/issues/271
    - https://github.com/webextension-toolbox/webextension-toolbox/pull/347

---
+++ webextension-toolbox-config.js
@@ -0,0 +1,36 @@
+// This file is not going through babel transformation.
+// So, we write it in vanilla JS
+// (But you could use ES2015 features supported by your Node.js version)
+const webpack = require("webpack");
+
+module.exports = {
+  webpack: (config, { dev, vendor }) => {
+    // Perform customizations to webpack config
+
+    // TODO(FAP): can be removed once https://github.com/webextension-toolbox/webextension-toolbox/pull/347 is merged
+    // Add webextension polyfill
+    if (["edge"].includes(vendor)) {
+      config.plugins.push(
+        new webpack.ProvidePlugin({
+          browser: require.resolve("webextension-polyfill"),
+        })
+      );
+
+      // The webextension-polyill doesn't work well with webpacks ProvidePlugin.
+      // So we need to monkey patch it on the fly
+      // More info: https://github.com/mozilla/webextension-polyfill/pull/86
+      config.module.rules.push({
+        test: /webextension-polyfill[\\/]+dist[\\/]+browser-polyfill\.js$/,
+        loader: require.resolve("string-replace-loader"),
+        query: {
+          search: 'typeof browser === "undefined"',
+          replace:
+            'typeof window.browser === "undefined" || Object.getPrototypeOf(window.browser) !== Object.prototype',
+        },
+      });
+    }
+
+    // Important: return the modified config
+    return config;
+  },
+};
