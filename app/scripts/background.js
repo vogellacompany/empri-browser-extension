56abfc80de7932d6db5ff006ce40064debf2050f
Christian Burkert <burkert@informatik.uni-hamburg.de>
2021-03-04 00:00:00 +0100

    Update backgroud script to new config

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -2,11 +2,8 @@ browser.storage.sync.get("ghrOn").then((res) => {
   if (typeof res.ghrOn == "undefined") {
     browser.storage.sync.set({
       ghrOn: true,
-      ghrRedactMonth: false,
-      ghrRedactDay: false,
-      ghrRedactHours: true,
-      ghrRedactMinutes: true,
-      ghrRedactSeconds: true,
+      mostsigunit: "year",
+      studyOptIn: false,
     });
   } else if (!res.ghrOn) {
     browser.browserAction.setIcon({
