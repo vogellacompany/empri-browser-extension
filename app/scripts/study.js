9b1afafc79c1904042de341c6c1e7225804cc640
Christian Burkert <burkert@informatik.uni-hamburg.de>
2021-03-08 00:00:00 +0100

    Store msu choices in format that is used for reporting (#4)
    - including days-since-opt-in (#3)

--- app/scripts/study.js
+++ app/scripts/study.js
@@ -1,6 +1,45 @@
 import { DateTime } from "luxon";
 import { v4 as uuidv4 } from 'uuid';
-import { mapreplacer, mapreviver } from "./utils.js";
+
+
+class MsuChoiceRecord {
+  constructor(daysSince, url, tsType, msu, frequency = 0) {
+    this.daysSinceOptIn = daysSince;
+    this.url = url;
+    this.xpath = tsType;
+    this.mostSignificantUnit = msu;
+    this.frequency = frequency;
+  }
+
+  inc() {
+    this.frequency++;
+  }
+
+  matches(other) {
+    return this.daysSinceOptIn == other.daysSinceOptIn &&
+      this.url == other.url &&
+      this.xpath == other.xpath &&
+      this.mostSignificantUnit == other.mostSignificantUnit;
+  }
+
+  static from(json){
+    return Object.assign(new MsuChoiceRecord(), json);
+  }
+}
+
+
+class Report {
+  constructor(partId) {
+    this.participantIdentifier = partId;
+    this.entries = [];
+  }
+
+  static from(json){
+    let report = new Report(json.participantIdentifier);
+    report.entries = Array.from(json.entries, MsuCoiceRecord.from);
+    return report;
+  }
+}
 
 
 // Random participant identifier
@@ -51,45 +90,46 @@ export function resetStudyData() {
 }
 
 
-export function getDaysSinceOptIn(callback) {
-  browser.storage.sync.get("studyOptInDate").then((res) => {
-    let optInDateRaw = res.studyOptInDate;
-    if (optInDateRaw) {
-      let optInDate = DateTime.fromFormat(optInDateRaw, "yyyy-MM-dd");
-      let daysSinceOptIn = Math.trunc(optInDate.diffNow("days").days);
-      callback(daysSinceOptIn);
-    }
-  });
+export function calcDaysSince(date) {
+  let datetime = DateTime.fromFormat(date, "yyyy-MM-dd");
+  return -Math.trunc(datetime.diffNow("days").days);
 }
 
 
 export function updateStudyData(tsType, msu) {
   // increment counter in local storage area
-  browser.storage.local
-    .get("msuChoices") // {TSTYPE: {MSU: Counter, ...}, ...}
-    .then((res) => {
-      var msuChoices = res.msuChoices;
-      if (msuChoices === undefined) {
-        msuChoices = new Map();
-      } else {
-        msuChoices = JSON.parse(msuChoices, mapreviver);
-      }
+  Promise.all([
+    browser.storage.local.get("msuChoices"),
+    browser.storage.sync.get("studyOptInDate"), // to calc daysSince
+  ])
+  .then((results) => {
+    let local = results[0];
+    let sync = results[1];
 
-      var tsStats = msuChoices.get(tsType);
-      if (tsStats === undefined) {
-        tsStats = new Map();
-      }
-      var tsMsuCount = tsStats.get(msu);
-      if (tsMsuCount === undefined) {
-        tsMsuCount = 0;
-      }
-      tsMsuCount++;
-      tsStats.set(msu, tsMsuCount);
-      msuChoices.set(tsType, tsStats);
-      let stringified = JSON.stringify(msuChoices, mapreplacer);
-      console.log(`LS: msuChoices ${stringified}`);
+    if (local.msuChoices === undefined) {
+      local.msuChoices = [];
+    }
 
-      // store updated stats
-      browser.storage.local.set({ msuChoices: stringified });
-    });
+    let daysSince = calcDaysSince(sync.studyOptInDate);
+    let newChoice = new MsuChoiceRecord(daysSince, "", tsType, msu);
+
+    // reuse existing matching choice or add new one
+    let msuChoices = Array.from(local.msuChoices, MsuChoiceRecord.from);
+    let matchingRecord = msuChoices.find(newChoice.matches, newChoice);
+    if (matchingRecord === undefined) {
+      msuChoices.push(newChoice);
+      matchingRecord = newChoice;
+      console.log("No match");
+    } else {
+      console.log(`Matched ${JSON.stringify(matchingRecord)}`);
+    }
+
+    // increment choice frequency
+    matchingRecord.inc();
+    console.log(`LS: msuChoices ${JSON.stringify(msuChoices)}`);
+
+    // store updated stats
+    return browser.storage.local.set({ msuChoices: msuChoices });
+  })
+  .catch(error => console.error(error));
 }
