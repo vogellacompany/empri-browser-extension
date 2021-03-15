fd2f4105f08449ed5e96f2f54b5f48c5cd0c0dda
Christian Burkert <burkert@informatik.uni-hamburg.de>
2021-03-16 00:00:00 +0100

    Running mean and variance

---
+++ app/scripts/stats.js
@@ -0,0 +1,42 @@
+export class RunningStats {
+  // Online variance calculation based on Welford.
+  // cf. https://www.johndcook.com/blog/standard_deviation/
+  constructor(count = 0, mean = 0, v = 0) {
+    this.count = 0
+    this._mean = 0
+    this._v = 0
+  }
+
+  update(x) {
+    this.count++
+    if (this.count == 1) {
+      this._mean = x // first value
+    } else {
+      let prev_mean = this._mean
+      this._mean += (x - this._mean) / this.count
+      this._v += (x - prev_mean) * (x - this._mean)
+    }
+  }
+
+  get mean() {
+    this.validate()
+    return this._mean
+  }
+
+  get variance() {
+    if (this.count < 2) {
+      return Number.NaN
+    }
+    return this._v / (this.count - 1)
+  }
+
+  get std() {
+    return Math.sqrt(this.variance)
+  }
+
+  validate() {
+    if (this.count == 0) {
+      throw new Error('Mean is undefined')
+    }
+  }
+}
