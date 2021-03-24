c92f3054b1e3ba67dfc8c12660cc6fbf7106ba25
Christian Burkert <burkert@informatik.uni-hamburg.de>
2021-03-25 00:00:00 +0100

    Onboarding WIP

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -14,3 +14,17 @@ browser.storage.sync.get("ghrOn").then((res) => {
     });
   }
 });
+
+browser.runtime.onInstalled.addListener(async ({ reason, temporary }) => {
+  if (temporary) return; // skip during development
+  switch (reason) {
+    case "install":
+      {
+        const url = browser.runtime.getURL("pages/welcome.html");
+        //await browser.tabs.create({ url });
+        await browser.windows.create({ url, type: "popup", height: 600, width: 600, });
+      }
+      break;
+    // see below
+  }
+});
